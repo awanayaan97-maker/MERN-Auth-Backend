05531e8beb49f57412853c9f1b923aa4f607bd93
ayaan waheed <awanayaan97@gmail.com>
2026-08-27 13:09:46 +0500

    OK

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ const authRoute = require("./Routes/authRoutes")
 const userRoute = require("./Routes/userRoutes")
 
 
-app.use(cors({
-    origin: [
-        process.env.API_URL,
-        "http://localhost:5173"
-    ]
+aapp.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version', 'Authorization'],
+    credentials: true
 }));
 
 app.use(express.json())
