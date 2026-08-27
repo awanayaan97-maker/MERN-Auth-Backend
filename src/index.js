9925097d60dd5d7bfb16ff88ec598661a11e4ba0
ayaan waheed <awanayaan97@gmail.com>
2026-08-27 12:49:59 +0500

    OK

--- src/index.js
+++ src/index.js
@@ -14,13 +14,18 @@ const connectDB = require("./Config/db");
 const authRoute = require("./Routes/authRoutes")
 const userRoute = require("./Routes/userRoutes")
 
-app.use(cors())
+app.use(cors({
+    origin: [
+      process.env.API_URL,
+      "http://localhost:5173/"
+    ]
+}))
 app.use(express.json())
 
 connectDB()
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome To Our Website </h1>")
+    res.send("<h1>Welcome To Our Website </h1>")
 })
 
 app.use("/api/auth", authRoute)
@@ -29,5 +34,5 @@ app.use("/api/user", userRoute)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server is working on Port ${PORT}`);  
+    console.log(`Server is working on Port ${PORT}`);
 })
